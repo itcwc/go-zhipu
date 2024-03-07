22fec971899153dc902a861a0e9191cec0227d29
it_cwc <1766987395@qq.com>
2024-03-07 13:47:56 +0800

    post 方法调整

--- utils/http_client.go
+++ utils/http_client.go
@@ -11,53 +11,54 @@ import (
 
 // Post 发送HTTP POST请求并获取响应结果
 func Post(apiURL, token string, params interface{}, timeout time.Duration) (map[string]interface{}, error) {
-
 	// 将请求参数转换为 JSON 格式
 	jsonParams, err := json.Marshal(params)
-
 	if err != nil {
 		return nil, fmt.Errorf("编码请求参数失败: %v", err)
 	}
+
 	// 创建请求
-	res, err := http.NewRequest("POST", apiURL, bytes.NewBuffer(jsonParams))
+	req, err := http.NewRequest("POST", apiURL, bytes.NewBuffer(jsonParams))
 	if err != nil {
 		return nil, fmt.Errorf("创建请求失败: %v", err)
 	}
 
 	// 设置请求头的 Authorization 字段
-	res.Header.Set("Accept", "application/json")
-	res.Header.Set("Content-Type", "application/json; charset=UTF-8")
-	res.Header.Set("Authorization", token)
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	req.Header.Set("Authorization", token)
 
 	// 创建一个具有指定超时时间的 HTTP 客户端
 	client := http.Client{Timeout: timeout}
 
 	// 发送请求
-	resp, err := client.Do(res)
+	resp, err := client.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("发送请求失败: %v", err)
 	}
 	defer resp.Body.Close()
 
-	// 读取响应体内容
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("读取响应体失败: %v", err)
-	}
+	// 检查响应状态码
+	if resp.StatusCode == http.StatusOK {
+		// 读取响应体内容
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, fmt.Errorf("读取响应体失败: %v", err)
+		}
 
-	// 检查响应状态码是否为 200
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("意外响应状态码: %s", body)
-	}
-	// 解析响应结果
-	var response map[string]interface{}
-	err = json.Unmarshal(body, &response)
-	if err != nil {
-		return nil, fmt.Errorf("解码响应失败: %v", err)
-	}
+		// 解析响应结果
+		var response map[string]interface{}
+		err = json.Unmarshal(body, &response)
+		fmt.Println(string(body))
+		if err != nil {
+			return nil, fmt.Errorf("解码响应失败: %v", err)
+		}
 
-	// 返回响应结果
-	return response, nil
+		// 返回响应结果
+		return response, nil
+	}
+	// 处理非 200 状态码的情况
+	return nil, fmt.Errorf("意外响应状态码: %d", resp.StatusCode)
 }
 
 func Stream(apiURL, token string, params interface{}, timeout time.Duration) (*http.Response, error) {
